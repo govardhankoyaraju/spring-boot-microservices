c3dfbf055c22c9173963112a75f0b31fe43c6fb4
Govardhan <govardhan.d369@gmail.com>
2022-06-20 22:45:26 +0530

    Initial commit

---
+++ customer/src/main/java/com/vardhan/customer/Customer.java
@@ -0,0 +1,14 @@
+package com.vardhan.customer;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Customer {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
---
+++ customer/src/main/java/com/vardhan/customer/CustomerService.java
@@ -0,0 +1,17 @@
+package com.vardhan.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest registrationRequest) {
+        Customer customer = Customer.builder()
+                .firstName(registrationRequest.firstName())
+                .lastName(registrationRequest.lastName())
+                .email(registrationRequest.email())
+                .build();
+        // TODO: check if email is valid
+        // TODO: check if email is not taken
+        // TODO: store customer in db
+    }
+}
