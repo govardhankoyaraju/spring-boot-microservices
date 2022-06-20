b697965ac8446039e4c2775b08f84eb0fc18ae70
Govardhan <govardhan.d1194@gmail.com>
2022-06-20 23:41:31 +0530

    Database connection

--- customer/src/main/java/com/vardhan/customer/CustomerService.java
+++ customer/src/main/java/com/vardhan/customer/CustomerService.java
@@ -3,7 +3,7 @@ package com.vardhan.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest registrationRequest) {
         Customer customer = Customer.builder()
                 .firstName(registrationRequest.firstName())
@@ -13,5 +13,6 @@ public record CustomerService() {
         // TODO: check if email is valid
         // TODO: check if email is not taken
         // TODO: store customer in db
+        customerRepository.save(customer);
     }
 }
